19642120ac6984884fc0dbaa5fa49e1e6c4ce6b0
ibrahimshamma99 <i.abushammah@gmail.com>
2020-03-04 13:06:42 +0200

    update

--- Apply.py
+++ Apply.py
@@ -1,5 +1,6 @@
 from LinearRegression.LinearReg import LinearReg
 from sklearn.datasets import load_boston
+from LogisticRegression.LogisticReg import LogisticReg
 import numpy as np
 
 dataset = load_boston()
@@ -8,4 +9,8 @@ Y = dataset.target[:,np.newaxis]
 
 linreg = LinearReg()
 linreg.run(X,Y)
-print(linreg.)
+linreg.plotError()
+
+LogReg = LogisticReg()
+LogReg.run(X , Y)
+LogReg.plotError()
